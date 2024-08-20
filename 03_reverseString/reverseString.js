92baf6653cda6c37da2aafca0d845879eea013c5
Leo Pinho <leonardo.zoot@gmail.com>
2024-08-20 16:09:59 -0300

    ex 01, 02 and 03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const length = string.length;
+    let reversedString = '';
+    for (let i = 0; i < length; i++) {
+        reversedString = reversedString.concat('', string.charAt(length - (1 + i)))
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
