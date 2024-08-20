d6392f0701c2061dfbb344ee508576910d8a5632
Leo Pinho <leonardo.zoot@gmail.com>
2024-08-20 17:19:57 -0300

    started exerc 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+
+//receives array and target
+//identify first target on targets array
+//loop over array and remove every instance of the current target
+//repeat last two steps for each target
+//return final array
+
+
+const removeFromArray = function(array, ...targets) {
+    let finalArray = array;
+    
+    console.log(targets)
+    return finalArray;
 };
 
+removeFromArray([1, 2, 3, 4], 3)
 // Do not edit below this line
-module.exports = removeFromArray;
+// module.exports = removeFromArray;
