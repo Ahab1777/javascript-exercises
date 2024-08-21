489b6731b561b4602467be41af5b7ceae6874bdb
Leo Pinho <leonardo.zoot@gmail.com>
2024-08-21 09:33:27 -0300

    exerc 04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,32 @@
 
 //receives array and target
-//identify first target on targets array
-//loop over array and remove every instance of the current target
+//identify value to be removed
+//loop over target array and remove every instance of the value to be removed
 //repeat last two steps for each target
 //return final array
 
 
-const removeFromArray = function(array, ...targets) {
-    let finalArray = array;
-    
-    console.log(targets)
-    return finalArray;
+const removeFromArray = function(targetGroup, ...valuesToBeRemoved) {
+    console.log('targetGroup:', targetGroup, 'valuesToBeRemoved:', valuesToBeRemoved)
+    let counter = 0;
+    //current value to be removed
+    while (counter < valuesToBeRemoved.length) {
+        let counter2 = 0;
+        //loop over target group to remove each item matching the current value to be removed
+        while (counter2 < targetGroup.length) {
+            if (targetGroup[counter2] === valuesToBeRemoved[counter]) {
+                targetGroup.splice(counter2, 1)
+            } else {
+                counter2++
+            }
+        }
+        counter++
+    }
+    console.log(targetGroup)
+    return targetGroup;
 };
 
-removeFromArray([1, 2, 3, 4], 3)
+removeFromArray([1, 2, 2, 3], 2)
+
 // Do not edit below this line
-// module.exports = removeFromArray;
+module.exports = removeFromArray;
