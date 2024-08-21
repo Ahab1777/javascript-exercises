381e1e08e6a5e87d64333b47ec7ad5c59b9342f8
Leo Pinho <leonardo.zoot@gmail.com>
2024-08-21 11:34:29 -0300

    exerc 07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  celsius = (fahrenheit - 32) * (5 / 9);
+  //round it
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius;
 };
+//fahrenheit to celsius
+//x °F ≘ (x − 32) × ⁠5/9⁠ °C
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  fahrenheit = celsius * (9/5) + 32;
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
@@ -9,3 +18,7 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+//celsius to fahrenheit
+//x °C ≘ (x × ⁠9/5⁠ + 32) °F
+
