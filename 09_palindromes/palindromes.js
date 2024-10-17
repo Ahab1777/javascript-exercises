ab6c67660ebe639ea23342b1aed3b5f8f52873a3
Leo Pinho <leonardo.zoot@gmail.com>
2024-10-17 18:46:31 -0300

    exerc 09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,45 +1,47 @@
-//remove every unit that is not a letter from the string
-    //filter out every non-letter by comparing to unicode number range (97 - 122  / a - z) 
-    // function isLetter(str) {
-    //  return str.length === 1 && str.match(/[a-z]/i);
-    //   }
-      
-    //convert string to lowercase
-//create a reversed copy of the string
-//compare original and reversed string
+//cast everything to lower case
+//remove all non-letters and non-number items
+//reverse string
+//compare strings to check for palindrome
 
 const palindromes = function (word) {
-
-  const isItALetter = (letter) => {
-    const unicodeChar = letter.charCodeAt(0) 
-    return (unicodeChar >= 97 && unicodeChar <= 122);
-  }
-
-  const wordToArray = [...word];
-  const palindrome = [];
-
-  //convert everything to lower case
-  const lowerCaseArray = wordToArray.map(item => item.toLowerCase())
-
-  //remove non-letters
-  const onlyLettersArray = lowerCaseArray.filter(isItALetter)
-  //console.log(onlyLettersArray)
-  //reverse the string
-  let reversedWord = [...onlyLettersArray]
-  reversedWord.reverse()
-  //console.log(`only letters = ${onlyLettersArray}`)
-  //console.log(`reversedWord post reverse = ${reversedWord}`)
-  console.log(onlyLettersArray)
-  console.log(reversedWord)
-
-  for (let i = 0 ; i < onlyLettersArray.length ; i++) {
-    if (onlyLettersArray[i] !== reversedWord[i]) {
-      return false
+  
+  //function to remove all non-letter and non-number digits
+  const wordCleaner = function (supposedPalindrome) {
+    const cleanedWord = []
+    const lowerCasedSupposedPalindrome = supposedPalindrome.toLowerCase()
+    for (let i = 0 ; i < lowerCasedSupposedPalindrome.length; i++){
+      //convert each letter to lower case
+      let target = lowerCasedSupposedPalindrome[i].toLowerCase().codePointAt(0)
+      //console.log(array[i]) 
+      if((target >= 97 && target <= 122) || (target >= 48 && target <= 57)){
+        //console.log(array[i])
+        cleanedWord.push(lowerCasedSupposedPalindrome[i])
+      }
     }
+    //console.log(cleanedWord)
+    return cleanedWord
   }
+
+  let cleanedWord = wordCleaner(word).join('')
+  
+  //reverse string
+  let reversedString = [...cleanedWord].reverse().join('')
+
+  console.log(reversedString)
+  console.log(cleanedWord)
+
+  if (reversedString === cleanedWord){
+    return true
+  } else return false
+
+
+  //let target = word.toLowerCase()
+
+
+
 };
 
-//palindromes('racecar');
+//palindromes('Racecar!');
 
 // Do not edit below this line
 module.exports = palindromes;
