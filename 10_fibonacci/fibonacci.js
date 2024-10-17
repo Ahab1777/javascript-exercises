648a87c119516676e2853b7b58c2892a32620765
Leo Pinho <leonardo.zoot@gmail.com>
2024-10-17 19:47:16 -0300

    exerc 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,37 @@
-const fibonacci = function() {
+//1, 1, 2, 3, 5, 8, etc.
 
+
+const fibonacci = function(target) {
+    if (target < 0) {
+        return "OOPS"
+    }
+    
+    if (target < 1){
+        return 0
+    }
+
+    let fibonacciArray = []
+    let fibonacciSum = 0
+    
+    for (let i = 0 ; i <= target ; i++){
+        if(i === 0){
+            fibonacciSum = 0
+        } else if (i === 1) {
+            fibonacciSum = 1
+        } else {
+            console.log('current sum =', fibonacciArray[i - 1], '+', fibonacciArray[i - 2])
+            fibonacciSum = (fibonacciArray[i - 1] + fibonacciArray[i - 2])
+            console.log('this is the sum done', fibonacciSum) 
+        }
+        fibonacciArray.push(fibonacciSum)
+
+    }
+    console.log('this is the array', fibonacciArray)
+    console.log('this is the target ', fibonacciArray[target])
+    return fibonacciArray[target]
 };
+
+//fibonacci(4)
 
 // Do not edit below this line
 module.exports = fibonacci;
